640a8c0b580530861e4e11a64a8a4aaa0bdec9a1
lavanya <lavanyace929@gmail.com>
2021-01-09 14:39:26 +0530

    Added:Gambling program

--- While-Loop/Gambling.sh
+++ While-Loop/Gambling.sh
@@ -1,13 +1,23 @@
 #!/bin/bash  -x
-
-RandomNum=$(( RANDOM%2 ))
 count=100;
-while [ $count <= 200 ]
+count1=100
+won=0
+lose=0
+bet=0
+while [ $count != 200 -a $count1 != 0 ] 
 do
-	if [ $RandomNum == 0 ]
-	then 
-		(($count--))
-	else
-		(($count++))
-	fi
+RandomNum=$(( RANDOM%2 ))
+((bet++))
+echo "No of bets made = $bet"
+if [ $RandomNum == 1 ]
+then 
+	((count++))
+		((won++))
+		echo "no of times won = $won"
+else
+	((count1--))
+		((lose++))
+		echo "no of times lose = $lose"
+fi
 done
+
