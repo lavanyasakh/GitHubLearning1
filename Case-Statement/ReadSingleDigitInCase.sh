d0e997a279dc2cade0255bb29997b2de4f07f24e
lavanya <lavanyace929@gmail.com>
2021-01-13 18:25:21 +0530

    Added:Read single Digit in Case

---
+++ Case-Statement/ReadSingleDigitInCase.sh
@@ -0,0 +1,45 @@
+#!/bin/bash -x
+
+read -p "Enter a number: " n
+fir=1;
+sec=2;
+trd=3;
+four=4;
+five=5;
+six=6;
+seven=7
+eight=8;
+nine=9;
+case $n in 
+	$fir)
+		echo "The given no is One"
+			;;
+	$sec)
+      echo "The given no is Two"		
+			;;
+	$trd)
+      echo "The given no is Three"
+			;;
+	$four)
+      echo "The given no is Four"
+			;;
+	$five)
+      echo "The given no is Five"
+			;;
+	$six)
+      echo "The given no is Six"
+			;;
+	$seven)
+      echo "The given no is Seven"
+			;;
+	$eight)
+      echo "The given no is Eight"
+			;;
+	$nine)
+      echo "The given no is Nine"
+			;;
+	*)
+		;;
+esac	
+
+
