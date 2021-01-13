9d595762b861c606ed3e4a478ef87263666159bd
lavanya <lavanyace929@gmail.com>
2021-01-13 11:25:24 +0530

    Updated AddRandomTwoDigit

--- Sequential-Statement/AddRandomTwoDigit.sh
+++ Sequential-Statement/AddRandomTwoDigit.sh
@@ -1,9 +1,12 @@
 #!/bin/bash -x
 sum=0;
-for i in `seq 1 5`
+read -p "Enter a num for iteration: " n
+for i in `seq 1 $n`
 do 
-	t=$(( RANDOM % 100 ))
+	t=$(( 10 + RANDOM%90 ))
 	sum=$(( $sum + $t ))
 	
 done
+Avg=$(($sum/n))
 echo $sum
+echo $Avg
