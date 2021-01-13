af5723fe9749f4797ad4a277935c4d658d695827
lavanya <lavanyace929@gmail.com>
2021-01-13 15:53:11 +0530

    Added:Updated SameDigits programm

--- Array-Practice/RepeatedNumbers.sh
+++ Array-Practice/RepeatedNumbers.sh
@@ -1,23 +1,13 @@
 #!/bin/bash -x
-count=0
-read -p "Enter a number: " n
-for (( i=0; i<$n; i++ ))
+
+num=11
+for (( i=1; i<$100; i++ ))
 do
-	read v
-	Numbers[$i]=$v
+	Data=$(($i%$num))
+	if [ $Data == 0 ]
+	then
+		Repeateddigits[i]=$i
+	fi
+	
 done
-echo ${Numbers[@]}
-for (( i=0; i<$n; i++ ))
-do
-	for (( j=i+1; j<$n; j++ ))
-	do
-		if [ ${Numbers[i]} == ${Numbers[j]} ]
-		then
-			echo ${Numbers[i]}
-			RepeatedNumbers[count]=${Numbers[i]}
-			((count++))
-			
-		fi
-	done
-done
-echo "The repeated numbers are: " ${RepeatedNumbers[@]}
+echo "The repeated digits  are: " ${Repeateddigits[@]}
