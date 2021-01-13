88a9ec541de4ba54cc9539e454dfc545c89c8126
lavanya <lavanyace929@gmail.com>
2021-01-13 12:09:49 +0530

    Updated Unit convertion

--- Sequential-Statement/UnitConvertion.sh
+++ Sequential-Statement/UnitConvertion.sh
@@ -1,7 +1,21 @@
 #!/bin/bash -x
+#inch to feet
+feet=12
+read -p "Enter no of inches X: " x
+Data1=$(( $x/$feet  ))
+echo "$x inches  is equal to $Data1 feet"
 
-read -p "Enter no of feets X: " x
-Data1=$(( $x * 12 ))
-echo "$x feet  is equal to $Data1 inches"
-Data2=$(( $x / 12 ))
-echo "$x inches  is equal to $Data2 feet"
+#feet to meter
+feet=0.3048
+read -p "Enter the length of rectangle: " l
+read -p "Enter the breadth of rectangle: " b
+Mul=$(($l*$b))
+Data2=$(($Mul*$feet))
+echo "$Mul is equal to $Data2 meters"
+
+#meter to acers
+meter=4047
+Mul=$(($Data2*25))
+Data3=$(($Mul/$meter))
+echo "$Mul is equal to $Data3 Acers"
+
