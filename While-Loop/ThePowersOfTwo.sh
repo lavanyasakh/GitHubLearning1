ea4f554322c930b57d27d5fb7d411b253f103a04
Lavanya <lavanyace929@gmail.com>
2021-01-14 23:25:23 +0530

    Added:The powers of two till a condition reached

--- While-Loop/ThePowersOfTwo.sh
+++ While-Loop/ThePowersOfTwo.sh
@@ -1,9 +1,14 @@
 #!/bin/bash -x 
 
-echo  "Enter a number: $1";
-
-for (( counter=0; counter<=$1; counter++ ))
+read -p "Enter a number: " n
+Max=256
+for (( counter=0; counter<=$n; counter++ ))
 do
 	Data=$((2**$counter))
-echo "$counter = $Data\n"
+   echo "$counter = $Data\n"
+	if [ $Data -eq $Max ]
+	then
+		break
+	fi		
+	
 done
